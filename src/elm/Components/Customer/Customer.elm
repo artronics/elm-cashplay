d452f15faa06ee04fbe300c8f04f0d9b8f540020
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2017-01-02 03:39:38 +0000

    rename Header to Customer there is no need for parent (customer) any more

--- src/elm/Components/Customer/Customer.elm
+++ src/elm/Components/Customer/Customer.elm
@@ -1,45 +1,89 @@
 module Components.Customer.Customer exposing (..)
 
-import Html exposing (Html, text)
+import Html exposing (Html, text, select, option)
+import Html.Attributes exposing (class)
 import Material
+import Material.Elevation as Elev
 import Material.Options exposing (..)
-import Material.Menu as Menu
-import Components.Customer.Header as Header exposing (..)
-
+import Material.Button as Button
+import Material.Icon as Icon
+import Components.Customer.SearchBar as SearchBar
+import Components.Customer.SearchList as SearchList
+import Components.Customer.NewCustomer as NewCustomer
+import Resources.Customer as Res
 
 type alias Customer =
-    { id : Int
-    , firstName : String
-    }
-
+    { id:Int
+    , firstName:String}
 
 type alias Model =
-    { header : Header.Model
+    { currentView : Maybe View
+    , query : SearchBar.Query
+    , searchBar : SearchBar.Model
+    , searchList : SearchList.Model
+    , newCustomer : NewCustomer.Model
     , mdl : Material.Model
     }
 
 
 init : Model
 init =
-    { header = Header.init
+    { currentView = Nothing
+    , query = { value = "", field = Res.Name }
+    , searchBar = SearchBar.init
+    , searchList = SearchList.init
+    , newCustomer = NewCustomer.init
     , mdl = Material.model
     }
 
 
+type View
+    = SearchList SearchBar.Query
+    | NewCustomer
+
+
 type Msg
-    = HeaderMsg Header.Msg
+    = ChangeView View
+    | SearchBarMsg SearchBar.Msg
+    | SearchListMsg SearchList.Msg
+    | NewCustomerMsg NewCustomer.Msg
     | Mdl (Material.Msg Msg)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        HeaderMsg msg_ ->
+        ChangeView (SearchList query) ->
             let
-                ( updatedHeader, cmd ) =
-                    Header.update msg_ model.header
+                (updatedSearchList,cmd) =
+                    SearchList.search query model.searchList
             in
-                ( { model | header = updatedHeader }, Cmd.map HeaderMsg cmd )
+            ( { model | currentView = Just (SearchList query), searchList = updatedSearchList }, Cmd.map SearchListMsg cmd )
+        ChangeView view ->
+            ( { model | currentView = Just view }, Cmd.none )
+
+
+        SearchBarMsg msg_ ->
+            -- query is what we get from SearchBar
+            let
+                ( updatedSearchBar, cmd, query ) =
+                    SearchBar.update msg_ model.searchBar
+            in
+                ( { model | searchBar = updatedSearchBar, query = query }, Cmd.map SearchBarMsg cmd )
+
+        SearchListMsg msg_ ->
+            let
+                ( updatedSearchList, cmd ) =
+                    SearchList.update msg_ model.searchList
+            in
+                ( { model | searchList = updatedSearchList }, Cmd.map SearchListMsg cmd )
+
+        NewCustomerMsg msg_ ->
+            let
+                ( updatedNewCustomer, cmd ) =
+                    NewCustomer.update msg_ model.newCustomer
+            in
+                ( { model | newCustomer = updatedNewCustomer }, Cmd.map NewCustomerMsg cmd )
 
         Mdl msg_ ->
             Material.update Mdl msg_ model
@@ -47,6 +91,34 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ Html.map HeaderMsg (Header.view model.header)
-        ]
+    let
+        subView =
+            case model.currentView of
+                Nothing ->
+                    div [] [ text "no view selected" ]
+
+                Just view ->
+                    case view of
+                        SearchList _->
+                            Html.map SearchListMsg (SearchList.view model.searchList)
+
+                        NewCustomer ->
+                            Html.map NewCustomerMsg (NewCustomer.view model.newCustomer)
+    in
+        div []
+            [ div [ Elev.e0, center, cs "art-page-header" ]
+                [ Html.map SearchBarMsg (SearchBar.view model.searchBar)
+                , Button.render Mdl
+                    [ 2 ]
+                    model.mdl
+                    [ Button.ripple, Button.raised, Button.primary, onClick (ChangeView (SearchList model.query)) ]
+                    [ Icon.i "search", text "Search" ]
+                , Button.render Mdl
+                    [ 3 ]
+                    model.mdl
+                    [ Button.ripple, Button.raised, css "margin-left" "50px", onClick (ChangeView NewCustomer) ]
+                    [ Icon.i "person_add", text "New Customer" ]
+                ]
+            , div [ Elev.e0, center ]
+                [ subView ]
+            ]
