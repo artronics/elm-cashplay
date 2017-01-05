d56b7a813db7abc24d878e536a5d2177c06c07da
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2017-01-05 13:55:04 +0000

    add searchBar

--- src/elm/Components/Customer/Customer.elm
+++ src/elm/Components/Customer/Customer.elm
@@ -16,6 +16,42 @@ import Components.Customer.ViewCustomer as ViewCustomer
 import Resources.Customer as Res
 import Components.Customer.SearchBar exposing (Query)
 
+type alias Model =
+    { searchBar : SearchBar.Model
+    , query : Maybe SearchBar.Query
+    , mdl : Material.Model
+    }
+
+init:Model
+init =
+    { searchBar = SearchBar.init
+    , query = Nothing
+    , mdl = Material.model
+    }
 
 
+type Msg
+    = SearchBarMsg SearchBar.Msg
+    | Mdl (Material.Msg Msg)
 
+update: Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        SearchBarMsg msg_ ->
+            -- SearchBar returns a query
+            updateSearchBar msg_ model
+        Mdl msg_ ->
+            Material.update Mdl msg_ model
+
+view: Model -> Html Msg
+view model =
+    div []
+        [ Html.map SearchBarMsg (SearchBar.view model.searchBar)]
+
+updateSearchBar: SearchBar.Msg -> Model -> (Model, Cmd Msg)
+updateSearchBar msg model =
+    let
+        (updatedSearchBar, cmd, query) =
+            SearchBar.update msg model.searchBar
+    in
+        ({model | searchBar = updatedSearchBar, query = Just query}, Cmd.map SearchBarMsg cmd)
