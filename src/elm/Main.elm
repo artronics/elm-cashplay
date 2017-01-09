c62d783ca1f34b915afe8cba8a8b6dd47103c2ea
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2017-01-09 18:21:41 +0000

    move componenets to parent

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,52 +1,27 @@
 module Main exposing (..)
 
 import Html exposing (Html, p, text)
-import Material
 import Material.Options exposing (..)
-import Components.Login as Login
-import Components.Tab as Tab
 
 
 type alias Model =
-    { tab : Tab.Model
-    , login : Login.Model
-    , mdl : Material.Model
-    }
+    {}
 
 
 model : Model
 model =
-    { tab = Tab.init
-    , login = Login.init
-    , mdl = Material.model
-    }
+    {}
 
 
 type Msg
-    = LoginMsg Login.Msg
-    | TabMsg Tab.Msg
-    | Mdl (Material.Msg Msg)
+    = NoOp
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        LoginMsg msg_ ->
-            let
-                ( login, cmd ) =
-                    Login.update msg_ model.login
-            in
-                ( { model | login = login }, Cmd.map LoginMsg cmd )
-
-        TabMsg msg_ ->
-            let
-                ( updatedTab, cmd ) =
-                    Tab.update msg_ model.tab
-            in
-                ( { model | tab = updatedTab }, Cmd.map TabMsg cmd )
-
-        Mdl msg_ ->
-            Material.update Mdl msg_ model
+        NoOp ->
+            ( model, Cmd.none )
 
 
 
@@ -60,9 +35,7 @@ view model =
         , div [ cs "art-main-row" ]
             [ div [ cs "art-actions" ] [ p [] [ text "Actions" ] ]
             , div [ cs "art-tabs" ]
-                [ Html.map LoginMsg <| Login.view model.login
-                , Html.map TabMsg (Tab.view model.tab)
-                ]
+                [ text "foo" ]
             , div [ cs "art-receipt" ] [ p [] [ text "Receipt" ] ]
             ]
         ]
