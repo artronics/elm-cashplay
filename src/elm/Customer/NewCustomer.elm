4f7cb7edf4467b8f6a1c5b888e19447f03f06dd1
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2017-01-18 16:14:46 +0000

    add img box for customer

--- src/elm/Customer/NewCustomer.elm
+++ src/elm/Customer/NewCustomer.elm
@@ -1,11 +1,12 @@
 module Customer.NewCustomer exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (placeholder, class)
+import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 import Customer.Customer exposing (Customer)
 import Customer.Messages exposing (Msg(..))
 import Customer.Models exposing (CustomerTab)
+import Views.Elements.Textfield as Txt exposing (txt)
 
 
 view : CustomerTab -> Html Msg
@@ -21,23 +22,49 @@ view customerTab =
 
 viewForm : CustomerTab -> Html Msg
 viewForm customerTab =
-    form [ class "form-horizontal" ]
-        [ div [ class "row" ]
-            [ halfInput "First Name" (\c i -> { c | firstName = i })
-            , halfInput "Last Name" (\c i -> { c | lastName = i })
+    Html.form [ class "form-horizontal" ]
+        [ div [ class "col-md-3" ] [ customerPic ]
+        , div [ class "col-md-9" ]
+            [ horInput "First Name" Full (OnNewCustomerInput (\c i -> { c | firstName = i }))
+            , horInput "Last Name" Full (OnNewCustomerInput (\c i -> { c | lastName = i }))
+            , horInput "Last Name" Full (OnNewCustomerInput (\c i -> { c | lastName = i }))
+            , horInput "Last Name" Full (OnNewCustomerInput (\c i -> { c | lastName = i }))
+              --            , txt "FirstName" [ onInput <| OnNewCustomerInput (\c i -> { c | firstName = i }) ] []
             ]
         ]
 
 
-halfInput : String -> (Customer -> String -> Customer) -> Html Msg
-halfInput lbl update =
-    div [ class "form-group col-sm-12 col-md-6" ]
+customerPic : Html msg
+customerPic =
+    div [ class "art-customer-pic well" ]
+        [ div [ class "art-upload-shot" ]
+            [ i [ class "fa fa-2x fa-camera" ] []
+            , i [ class "fa fa-2x fa-upload" ] []
+            ]
+        ]
+
+
+type Width
+    = Full
+    | Half
+
+
+horInput : String -> Width -> (String -> msg) -> Html msg
+horInput lbl width msg =
+    div
+        [ class "form-group col-sm-12"
+        , class <|
+            if width == Full then
+                "col-md-12"
+            else
+                "col-md-6"
+        ]
         [ label [ class "col-sm-3 control-label" ] [ text <| lbl ++ ":" ]
         , div [ class "col-sm-9" ]
             [ input
                 [ class "form-control"
                 , placeholder lbl
-                , onInput <| OnNewCustomerInput update
+                , onInput <| msg
                 ]
                 []
             ]
