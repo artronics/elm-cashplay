1a879559a030e74fae8feaf3e428a8d7d49aae0d
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2017-02-13 15:19:05 +0000

    add bootstrap 4 and tether and fontawesome

--- src/static/index.js
+++ src/static/index.js
@@ -1,8 +1,19 @@
 // pull in desired CSS/SASS files
 require( './styles/main.scss' );
 var $ = jQuery = require( '../../node_modules/jquery/dist/jquery.js' );           // <--- remove if jQuery not needed
-require( '../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.js' );   // <--- remove if Bootstrap's JS not needed 
+require( '../../node_modules/bootstrap/dist/js/bootstrap.js' );   // <--- remove if Bootstrap's JS not needed
+
+
+var jwt = "";
+if (localStorage.jwt){
+  jwt = localStorage.jwt;
+}
 
 // inject bundled Elm app into div#main
 var Elm = require( '../elm/Main' );
-Elm.Main.embed( document.getElementById( 'main' ) );
+var app = Elm.Main.embed( document.getElementById( 'main' ),{jwt:jwt} );
+
+app.ports.setLocalStorage.subscribe(function (keyVal) {
+  localStorage.removeItem(keyVal.key);
+  localStorage.setItem(keyVal.key,keyVal.value);
+})
