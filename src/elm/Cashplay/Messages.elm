32235863c39eb771e264c8b1db308fb9e224c26c
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2017-02-13 14:33:45 +0000

    from scratch

--- src/elm/Cashplay/Messages.elm
+++
@@ -1,9 +0,0 @@
-module Cashplay.Messages exposing (..)
-
-import Customer.Messages as Customer
-import Cashplay.Models exposing (Tab)
-
-
-type Msg
-    = SelectTab Tab
-    | CustomerTabMsg Customer.Msg
--- src/elm/Home/Messages.elm
+++
@@ -1,9 +0,0 @@
-module Home.Messages exposing (..)
-
-import Shared.Login as Login
-import Shared.Signup as Signup
-
-
-type Msg
-    = LoginMsg Login.Msg
-    | SignupMsg Signup.Msg
